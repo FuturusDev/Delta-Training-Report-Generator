7e575af51eabb8e4216c5edde794a00cd1e7f9e7
Colton Dale <colton.dale@futurus.com>
2024-02-15 13:40:28 -0500

    Initial creation of Delta Training report generator

--- Program.cs
+++ Program.cs
@@ -7,93 +7,37 @@ using System.Text;
 
 var localFilePath = ConfigurationManager.AppSettings["LocalPath"];
 var filePrepend = ConfigurationManager.AppSettings["FilePrepend"];
-var dayInterval = int.Parse(ConfigurationManager.AppSettings["DayInterval"] ?? "7");
+var dayInterval = int.Parse(ConfigurationManager.AppSettings["DayInterval"] ?? "14");
 var excludeFuturusData = Convert.ToBoolean(ConfigurationManager.AppSettings["ExcludeFuturusData"]);
 var dateFormat = "yyyyMMdd";
 var fullLocalFilePath = $"{localFilePath}/{filePrepend}_{DateTime.Now.ToString(dateFormat)}";
 
-var ExcludedFuturusData = excludeFuturusData ? "AND us.IsFuturus != 1" : string.Empty;
-var _getUniqueUsersLast7Days = @$"
-    SELECT u.ChrisId
-    FROM usersession us
-        JOIN user u ON (u.ChrisId = us.ChrisId)
-    WHERE us.StartTimeUtc >= DATE(NOW() - INTERVAL @DaysInThePast DAY) {ExcludedFuturusData}
-    GROUP BY u.ChrisId;";
-
-var _getSessionDataByUser = @$"
-    SELECT 
-	    u.FName AS FirstName,
-        u.LName AS LastName,
-        u.ChrisId,
-        us.StartTimeUtc AS StartTime,
-        us.DifficultyLevelId AS DifficultyLevel,
-        sm.Name AS SessionMode,
-        uss.StepId AS StepNumber,
-        s.Name AS StepName,
-        uss.Attempt AS Attempts,
-        uss.Duration,
-        sta.Name AS Status,
-        rs.ResultMessage
-    FROM usersession us
-	    JOIN user u ON (u.ChrisId = us.ChrisId)
-        JOIN sessionmode sm ON (us.SessionModeId = sm.SessionModeId)
-        JOIN usersessionstep uss ON (uss.UserSessionId = us.SessionId)
-        JOIN step s ON (s.StepId = uss.StepId)
-        LEFT JOIN usersessionstepresult ussr ON (ussr.UserSessionStepId = uss.UserSessionStepId)
-        LEFT JOIN resultstatus rs ON (ussr.ResultStatusId = rs.ResultStatusId)
-        LEFT JOIN status sta ON (rs.StatusId = sta.StatusId)
-    WHERE us.StartTimeUtc >= DATE(NOW() - INTERVAL @DaysInThePast DAY) AND u.ChrisId = @ChrisId {ExcludedFuturusData}
-    ORDER BY us.StartTimeUtc, uss.StepId, uss.Attempt;";
+var ExcludedFuturusData = /*excludeFuturusData*/false ? "AND us.IsFuturus != 1" : string.Empty; // TODO: Remove false and uncomment excludeFuturusData when implemented
 
 var _getAllSessionData = @$"
-    SELECT 
-	    u.FName AS FirstName,
-        u.LName AS LastName,
-        u.ChrisId,
-        us.StartTimeUtc AS StartTime,
-        us.DifficultyLevelId AS DifficultyLevel,
-        sm.Name AS SessionMode,
-        uss.StepId AS StepNumber,
-        s.Name AS StepName,
-        uss.Attempt AS Attempts,
-        uss.Duration,
-        sta.Name AS Status,
-        rs.ResultMessage
-    FROM usersession us
-	    JOIN user u ON (u.ChrisId = us.ChrisId)
-        JOIN sessionmode sm ON (us.SessionModeId = sm.SessionModeId)
-        JOIN usersessionstep uss ON (uss.UserSessionId = us.SessionId)
-        JOIN step s ON (s.StepId = uss.StepId)
-        LEFT JOIN usersessionstepresult ussr ON (ussr.UserSessionStepId = uss.UserSessionStepId)
-        LEFT JOIN resultstatus rs ON (ussr.ResultStatusId = rs.ResultStatusId)
-        LEFT JOIN status sta ON (rs.StatusId = sta.StatusId)
-    WHERE us.StartTimeUtc >= DATE(NOW() - INTERVAL @DaysInThePast DAY) {ExcludedFuturusData}
-    ORDER BY us.StartTimeUtc, uss.StepId, uss.Attempt;";
+    SELECT
+	    es.EmployeeId,
+        es.AppVersionNumber,
+	    sqr.SessionId,
+        q.QuizId,
+	    q.Name as QuizName,
+        sqr.EmployeeAnswer,
+        sqr.Score,
+        sqr.Attempt,
+        sqr.TimeStampUtc AS TimeStamp
+    FROM sessionquizresult sqr
+	    JOIN quiz q ON sqr.QuizId = q.QuizId
+        JOIN employeesession es ON es.SessionId = sqr.SessionId
+    WHERE es.StartTimeUtc >= DATE(NOW() - INTERVAL @DaysInThePast DAY) {ExcludedFuturusData}
+    ORDER BY TimeStampUtc ASC;";
 
-var connectionString = "server=mars-troubleshootingvr.mysql.database.azure.com;uid=marsadmin;pwd=RedZebra10202023330!;database=dbo";
+var connectionString = "server=delta-deicingvr.mysql.database.azure.com;uid=deltaadmin;pwd=FuturFile!5;database=dbo";
 using var connection = new MySqlConnection(connectionString);
 
 FileInfo fileInfo = new FileInfo(fullLocalFilePath);
 if (!fileInfo.Exists)
 {
     Directory.CreateDirectory(fullLocalFilePath);
-}
-
-var usersParameters = new DynamicParameters();
-usersParameters.Add("@DaysInThePast", dayInterval);
-var users = connection.Query<string>(_getUniqueUsersLast7Days, usersParameters);
-foreach (var item in users)
-{
-    var sessionDataByUserParameters = new DynamicParameters();
-    sessionDataByUserParameters.Add("@ChrisId", item);
-    sessionDataByUserParameters.Add("@DaysInThePast", dayInterval);
-    var sessionDataByUser = connection.Query<SessionData>(_getSessionDataByUser, sessionDataByUserParameters);
-    using (var writer = new StreamWriter($"{fullLocalFilePath}/{filePrepend}_{DateTime.Now.ToString(dateFormat)}_{item}.csv", false, Encoding.UTF8))
-    {
-        var csv = new CsvWriter(writer, CultureInfo.CurrentCulture);
-        csv.WriteRecords(sessionDataByUser);
-        csv.Flush();
-    }
 }
 
 var allSessionDataParameters = new DynamicParameters();
@@ -108,16 +52,13 @@ using (var writer = new StreamWriter($"{fullLocalFilePath}/{filePrepend}_{DateTi
 
 class SessionData
 {
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string ChrisId { get; set; }
-    public string StartTime { get; set; }
-    public string DifficultyLevel { get; set; }
-    public string SessionMode { get; set; }
-    public string StepNumber { get; set; }
-    public string StepName { get; set; }
-    public string Attempts { get; set; }
-    public string Duration { get; set; }
-    public string Status { get; set; }
-    public string ResultMessage { get; set; }
+    public string EmployeeId { get; set; }
+    public string AppVersionNumber { get; set; }
+    public string SessionId { get; set; }
+    public string QuizId { get; set; }
+    public string QuizName { get; set; }
+    public string EmployeeAnswer { get; set; }
+    public string Score { get; set; }
+    public string Attempt { get; set; }
+    public string TimeStamp { get; set; }
 }
